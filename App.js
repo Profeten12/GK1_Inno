1aa251475b30ecef7274165150ff91fb7c73bd18
Profeten12 <bepe23ad@student.cbs.dk>
2025-09-16 10:04:54 +0200

    template

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/HomeScreen";
+import ListScreen from "./src/ListScreen";
+import AboutScreen from "./src/AboutScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
